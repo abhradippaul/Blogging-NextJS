b050d116d0ac6a8ad5cdc2c33b03f9cd7c9af294
Abhradip Paul <111123303+abhradippaul@users.noreply.github.com>
2024-03-16 12:25:31 +0530

    Navbar and side navbar completed

---
+++ src/app/following/page.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function page() {
+  return (
+    <div>
+        followingPage
+    </div>
+  )
+}
+
+export default page
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,24 +1,118 @@
-"use client"
+"use client";
 import Link from "next/link";
-import React from "react";
 
 function Navbar() {
-  
+  // const [navControl, setNavControl] = useState<boolean>(false);
+  const hamburgerClick = () => {
+    const bgBlackElement = document.querySelector<Element>("#bg-black");
+    const sideNavbar = document.querySelector<Element>("#side-navbar");
+    if (bgBlackElement && sideNavbar) {
+      bgBlackElement.classList.toggle("hidden");
+      sideNavbar.classList.toggle("sideNavbar");
+    }
+  };
+
   return (
     <nav className="h-16 bg-slate-100 flex items-center">
-      <i className="fa-solid fa-bars mx-2 text-2xl cursor-pointer sm:text-4xl sm:mx-4"></i>
-      <div className="max-w-7xl w-full h-full m-auto px-2 flex items-center justify-between">
-        <div className="flex h-full items-center justify-between w-[20%] min-w-[90px] max-w-[150px]">
-          <i className="fa-brands fa-blogger text-2xl"></i>
-          <h1 className="text-xl sm:text-2xl"><Link href="/">E Blog</Link></h1>
+      <i
+        className="fa-solid fa-bars mx-2 text-2xl cursor-pointer sm:text-4xl sm:mx-4 z-10"
+        onClick={hamburgerClick}
+      ></i>
+      <div
+        id="bg-black"
+        className="opacity-30 bg-black absolute top-0 right-0 left-0 bottom-0 hidden"
+        onClick={hamburgerClick}
+      ></div>
+      <div
+        id="side-navbar"
+        className="absolute top-0 bottom-0 left-0 right-[50%] bg-slate-100 translate-x-[-100%] transformSideNavbar sm:right-[70%]"
+      >
+        <div className="h-16"></div>
+        <ul className="border text-xl p-4">
+          <li className="my-2">
+            <Link href="/" onClick={hamburgerClick}>
+              Home
+            </Link>
+          </li>
+          <li className="my-2">
+            <Link href="/following" onClick={hamburgerClick}>
+              Following
+            </Link>
+          </li>
+          <li className="my-2">
+            <Link href="/profile" onClick={hamburgerClick}>
+              Profile
+            </Link>
+          </li>
+        </ul>
+        <div className="border text-xl p-4">
+          <h1 className="my-2">Following</h1>
+          <ul>
+            <li className="my-2">
+              <Link href="/user1" onClick={hamburgerClick}>
+                user1
+              </Link>
+            </li>
+            <li className="my-2">
+              <Link href="/user2" onClick={hamburgerClick}>
+                user2
+              </Link>
+            </li>
+            <li className="my-2">
+              <Link href="/user3" onClick={hamburgerClick}>
+                user3
+              </Link>
+            </li>
+            <li className="my-2">
+              <Link href="/user4" onClick={hamburgerClick}>
+                user4
+              </Link>
+            </li>
+          </ul>
         </div>
-            <div className="flex items-center justify-between bg-white rounded-md overflow-hidden sm:w-1/2">
-              <input type="text" className="w-[80%] hidden text-base mx-2 border-none outline-none rounded-md px-2 py-1 sm:text:lg sm:block"/>
-              <i className="fa-solid fa-magnifying-glass text-xl m2-x cursor-pointer bg-slate-200 py-2 px-4 rounded-md sm:text-2xl"></i>
-            </div>
-            <div>
-                <Link href="/signin" className="border-2 text-xl border-green-500 px-4 py-1  rounded-md hover:bg-green-500 hover:text-white sm:text-2xl">Sign In</Link>
-            </div>
+        <div>
+          <h1>Explore</h1>
+          <ul>
+            <li>
+              <Link onClick={hamburgerClick} href="/science">
+                Science
+              </Link>
+            </li>
+            <li>
+              <Link onClick={hamburgerClick} href="/gaming">
+                Gaming
+              </Link>
+            </li>
+            <li>
+              <Link onClick={hamburgerClick} href="/technology">
+                Technology
+              </Link>
+            </li>
+          </ul>
+        </div>
+      </div>
+      <div className="max-w-7xl w-full h-full m-auto px-2 flex items-center justify-between">
+        <div className="flex h-full items-center justify-between w-[20%] min-w-[90px] max-w-[150px] z-10">
+          <i className="fa-brands fa-blogger text-2xl"></i>
+          <h1 className="text-xl sm:text-2xl">
+            <Link href="/">E Blog</Link>
+          </h1>
+        </div>
+        <div className="flex items-center justify-between bg-white rounded-md overflow-hidden sm:w-1/2">
+          <input
+            type="text"
+            className="w-[80%] hidden text-base mx-2 border-none outline-none rounded-md px-2 py-1 sm:text:lg sm:block"
+          />
+          <i className="fa-solid fa-magnifying-glass text-xl m2-x cursor-pointer bg-slate-200 py-2 px-4 rounded-md sm:text-2xl"></i>
+        </div>
+        <div>
+          <Link
+            href="/signin"
+            className="border-2 text-xl border-green-500 px-4 py-1  rounded-md hover:bg-green-500 hover:text-white sm:text-2xl"
+          >
+            Sign In
+          </Link>
+        </div>
       </div>
     </nav>
   );
